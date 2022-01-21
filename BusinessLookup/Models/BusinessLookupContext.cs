4642dbe5d73957370b6dd3183acc17d595b80c04
Kaxon <karenaxon@users.noreply.github.com>
2022-01-21 09:23:01 -0800

    Add BusinessLookupContext to project.

---
+++ BusinessLookup/Models/BusinessLookupContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLookup.Models
+{
+    public class BusinessLookupContext : DbContext
+    {
+        public BusinessLookupContext(DbContextOptions<BusinessLookupContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Business> Businesses { get; set; }
+    }
+}
