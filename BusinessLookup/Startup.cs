962b620a568536084c0a9c3b1611559be6ae246d
Kaxon <karenaxon@users.noreply.github.com>
2022-01-21 11:18:24 -0800

    Edit Startup file.

--- BusinessLookup/Startup.cs
+++ BusinessLookup/Startup.cs
@@ -17,7 +17,6 @@ namespace BusinessLookup
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
 
@@ -26,7 +25,6 @@ namespace BusinessLookup
       services.AddControllers();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
