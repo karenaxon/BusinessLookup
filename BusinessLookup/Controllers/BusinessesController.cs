e64ced955d049bfa12170cf59828747f0006d97f
Kaxon <karenaxon@users.noreply.github.com>
2022-01-21 14:21:16 -0800

    Add update functionality.

--- BusinessLookup/Controllers/BusinessesController.cs
+++ BusinessLookup/Controllers/BusinessesController.cs
@@ -1,4 +1,6 @@
+using System.Security.AccessControl;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -43,6 +45,39 @@ namespace BusinessLookup.Controllers
       }
 
       return business;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(business).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if(!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    private bool BusinessExists(int id)
+    {
+      return _db.Businesses.Any(e => e.BusinessId == id);
     }
   }
 }
