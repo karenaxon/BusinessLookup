ae57c672fa045517980c1d552c41fb43209b1050
Kaxon <karenaxon@users.noreply.github.com>
2022-01-21 15:59:00 -0800

    Add validation to properties.

--- BusinessLookup/Models/Business.cs
+++ BusinessLookup/Models/Business.cs
@@ -1,15 +1,35 @@
+using System.Security.AccessControl;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BusinessLookup.Models
 {
   public class Business
   {
     public int BusinessId  { get; set; }
+
+    [Required]
+    [StringLength(255, ErrorMessage = "A business name is required.")]
     public string Name { get; set; }
+
+    [Required]
+    [StringLength(40, ErrorMessage = "A business type is required.")]
     public string Type { get; set; }
+
+    [Required]
+    [StringLength(255, ErrorMessage = "A street address is required.")]
     public string StreetAddress { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "A city is required.")]
     public string City { get; set; }
+
+    [Required]
+    [StringLength(2, ErrorMessage = "State is required. Use State abbreviations.")]
     public string State { get; set; }
+
+    [Required]
+    [StringLength(5, ErrorMessage = "Zip code is required and must be 5 digits.")]
     public string ZipCode { get; set; }
   }
 }
